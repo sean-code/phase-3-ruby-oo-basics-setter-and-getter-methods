d1bbd99a68ca8dfcd0f6d21a488e2b1c70ad208a
sean-code <john.nganga@student.moringaschool.com>
2022-10-20 10:57:22 +0300

    Lab completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    attr_reader :name
+    attr_accessor :job
 
+    def name=(name)
+        @name = name
+    end
+    
+    # def person
+    #     @name
+    # end
+
+    # def job=(job)
+    #     @job = job
+    # end
+
+    # def job
+    #     @job
+    # end
 end
